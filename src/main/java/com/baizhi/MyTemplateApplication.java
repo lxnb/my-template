d0205e7843e341a07a8a7f4c8519aacf64795faf
lx <45872595+lxnb@users.noreply.github.com>
2018-12-14 20:09:31 +0800

    Update MyTemplateApplication.java

--- src/main/java/com/baizhi/MyTemplateApplication.java
+++ src/main/java/com/baizhi/MyTemplateApplication.java
@@ -10,6 +10,9 @@ public class MyTemplateApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyTemplateApplication.class, args);
+        
+        
+        
     }
 
 }
