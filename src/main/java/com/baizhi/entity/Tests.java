551ff0d2a32c427b286777059316b7aa49f0503d
lx <1546255703@qq.com>
2018-12-17 19:02:25 +0800

    "test update"

--- src/main/java/com/baizhi/entity/Tests.java
+++ src/main/java/com/baizhi/entity/Tests.java
@@ -1,7 +1,6 @@
 package com.baizhi.entity;
 
 import com.alibaba.fastjson.annotation.JSONField;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
